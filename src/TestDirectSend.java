5a0e1a0f8e2cc676bf80f0e2c46fe9c0739a580d
Cameron Clark <cameron.clark1@my.jcu.edu.au>
2017-10-07 12:53:03 +1000

    add directUDP test, send and receiver move old tests to oldTests

---
+++ src/TestDirectSend.java
@@ -0,0 +1,19 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class TestDirectSend {
+    public static void main(String[] args) throws Exception{
+        // fixes mac OS bug with wireless internet use
+        System.setProperty("java.net.preferIPv4Stack", "true");
+
+        DatagramSocket socket = new DatagramSocket(49152);
+        String message = "hello!";
+
+        InetAddress address = InetAddress.getByName("10.139.96.80");
+        DatagramPacket packet = new DatagramPacket(message.getBytes(), message.length(), address, 49152);
+        socket.send(packet);
+        socket.close();
+
+    }
+}
---
+++ src/TestDirectUDP.java
@@ -0,0 +1,26 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class TestDirectUDP {
+    public static void main(String[] args) throws Exception{
+        // fixes mac OS bug with wireless internet use
+        System.setProperty("java.net.preferIPv4Stack", "true");
+
+        InetAddress localIP = InetAddress.getLocalHost();
+        System.out.println("my ip is: " + localIP.getHostAddress());
+
+        DatagramSocket socket = new DatagramSocket(49152);
+
+        byte[] messageBuffer = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket(messageBuffer, 1024);
+
+        socket.receive(receivePacket);
+
+        String message = new String(messageBuffer).trim();
+        System.out.println(message);
+
+        socket.close();
+
+    }
+}
