34d9641d82090dd5b5d44d006c7770fea1c7cc63
Cameron Clark <cameron.clark1@my.jcu.edu.au>
2017-10-12 15:09:56 +1000

    server replies to client with OK once added, else give error

--- src/DirectUDP.java
+++ src/DirectUDP.java
@@ -20,33 +20,18 @@ public class DirectUDP {
         socket.close();
         return message;
 
-//        //test bs
-//
-//        System.setProperty("java.net.preferIPv4Stack", "true");
-//
-//        InetAddress localIP = InetAddress.getLocalHost();
-//        System.out.println("my ip is: " + localIP.getHostAddress());
-//
-//        DatagramSocket socket = new DatagramSocket(49152);
-//
-//        byte[] messageBuffer = new byte[1024];
-//        DatagramPacket receivePacket = new DatagramPacket(messageBuffer, 1024);
-//
-//        socket.receive(receivePacket);
-//
-//        String message = new String(messageBuffer).trim();
-//        System.out.println(message);
-//
-//        socket.close();
     }
 
-    public static void send(int port, int myPort, String ip, String message) throws Exception {
+    public static void send(int receiver, int sender, String ip, String message) throws Exception {
         System.setProperty("java.net.preferIPv4Stack", "true");
 
-        DatagramSocket socket = new DatagramSocket(myPort);
+        //add receiver and sender to packet
+        message = message + " " + receiver + " " + sender;
+
+        DatagramSocket socket = new DatagramSocket(sender);
 
         InetAddress address = InetAddress.getByName(ip);
-        DatagramPacket packet = new DatagramPacket(message.getBytes(), message.length(), address, port);
+        DatagramPacket packet = new DatagramPacket(message.getBytes(), message.length(), address, receiver);
         socket.send(packet);
         socket.close();
 
